abff7e1bdee0b59841ae99455233e319b9f5ff97
Dilasha Upadhyay <dilashaupa@gmail.com>
2024-01-13 15:06:11 +0545

    final

--- final.py
+++ final.py
@@ -47,20 +47,27 @@ class Particle:
     def __init__(self):
         self.position = [random.uniform(-0.5, 0.5) for _ in range(3)]  # Random initial position
         self.velocity = [random.uniform(-0.1, 0.1) for _ in range(3)]  # Random initial velocity
+        self.color = self.generate_random_color()  # Random RGB color
+        self.radius = 0.02
 
     def update(self):
         for i in range(3):
             self.position[i] += self.velocity[i]
             # Bound the particles within the cube
-            if abs(self.position[i]) >= 1:
+            if self.position[i] + self.radius >= 1 or self.position[i] - self.radius <= -1:
                 self.velocity[i] *= -1
 
+    def generate_random_color(self):
+        return (random.random(), random.random(), random.random())
+
 # Create a list of particles
-num_particles = 100
+num_particles = 300  # Increase the number of particles
 particles = [Particle() for _ in range(num_particles)]
 
 # Function to draw the cube
 def draw_cube():
+    glLineWidth(3)  # Set line width to make the outline thicker
+    glColor3f(1, 1, 1)  # Set color to white
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
@@ -69,10 +76,10 @@ def draw_cube():
 
 # Function to draw particles
 def draw_particles():
-    glPointSize(2)
+    glPointSize(5)  # Increase the size of the particles
     glBegin(GL_POINTS)
-    glColor3f(1, 1, 1)  # Particle color
     for particle in particles:
+        glColor3fv(particle.color)  # Set particles to random color
         glVertex3fv(particle.position)
     glEnd()
 
@@ -87,6 +94,14 @@ while True:
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     draw_cube()
+
+    # Handle collisions continuously
+    for i in range(len(particles)):
+        for j in range(i + 1, len(particles)):
+            distance = sum((particles[i].position[k] - particles[j].position[k]) ** 2 for k in range(3))
+            if distance < (particles[i].radius + particles[j].radius) ** 2:
+                # Swap velocities for simplicity
+                particles[i].velocity, particles[j].velocity = particles[j].velocity, particles[i].velocity
 
     for particle in particles:
         particle.update()
